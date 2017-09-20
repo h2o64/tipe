97968f83cfec52b29c05a426a5521215215d5a1d
Louis Popi <theh2o64@gmail.com>
2017-09-20 16:37:58 +0200

    images: Remove color struct
    * Waste of time

--- Image_Reading/images.ml
+++ Image_Reading/images.ml
@@ -6,7 +6,6 @@ type matrix = float array array;;
 type bloc_size = int;;
 type image = {height : int ; width : int ; matrix : Graphics.color array array};;
 type bw_image = {height : int ; width : int ; mutable matrix : matrix};;
-type color = {mutable r : int ; mutable g : int ; mutable b : int ; mutable a : int};;
 
 (* Get height and width of an image *)
 let getHeight img = Array.length img;;
@@ -16,28 +15,19 @@ let getWidth img = Array.length img.(0);;
 let import_image file = let matrix = lire_image file in
 		({height = getHeight matrix; width = getWidth matrix; matrix = matrix} : image);;
 
-(* Convert RGB integer to color type *)
-let rgbint_to_color num =
-	(* Red/Green/Blue/transpArancy *)
-	let b = num mod 256 in
-	let g = (num/256) mod 256 in
-	let r = (num/256/256) mod 256 in
-	let a = (num/256/256/256) in
-	{r = r; g = g; b = b; a = a};;
-
-(* Convert GRB color to RGB color *)
-let grb_to_rgb cl = let x = cl.r in
-			cl.r <- cl.b;
-			cl.b <- x;;
-
-(* Convert color type to RGB integer *)
-let color_to_rgbint cl = cl.r + cl.g*256 + cl.b*256*256 + cl.a*256*256*256;;
-
 (* Get the right image format *)
 let getFormat height width =
 	let s_height = string_of_int height in
 	let s_width = string_of_int width in
 	String.concat "" [" ";s_height;"x";s_width];;
+
+(* Convert RGB integer to color type *)
+let rgbint_to_color num =
+	(* Red/Green/Blue *)
+	let b = num mod 256 in
+	let g = (num/256) mod 256 in
+	let r = (num/256/256) mod 256 in
+	(r,g,b);;
 
 (* Get pixel luminance *)
 let getLuminance pix =
@@ -49,13 +39,14 @@ let getLuminance pix =
 		* [1] Wikipédia - Relative luminance
 		* [2] W3.org - https://www.w3.org/TR/AERT#color-contrast
 		* [3] Darel Rex Finley - http://alienryderflex.com/hsp.html *)
-	 let cl = rgbint_to_color pix in
-	 let luminance = 0.299 *. (float_of_int (cl.r) ** 2.)
-		  					 +. 0.587 *. (float_of_int (cl.g) ** 2.)
-			 				 	 +. 0.114 *. (float_of_int (cl.b) ** 2.) in sqrt luminance;;
+	 let (r,g,b) = rgbint_to_color pix in
+	 let luminance = 0.299 *. (float_of_int (r) ** 2.)
+								+. 0.587 *. (float_of_int (g) ** 2.)
+								+. 0.114 *. (float_of_int (b) ** 2.) in sqrt luminance;;
 
 (* Convert RGB to greyscale *)
-let rgbToGreyScale color = (float_of_int (color.r + color.g + color.b))/.3.;;
+let rgbToGreyScale pix = let (r,g,b) = rgbint_to_color pix in
+			(float_of_int (r + g + b))/.3.;;
 
 (* Convert whole image to greyscale *)
 let imageToGreyScale (image : image) =
@@ -63,7 +54,7 @@ let imageToGreyScale (image : image) =
 		let ret = Array.make_matrix h w 0. in
 		for i = 0 to (h-1) do
 			for j = 0 to (w-1) do
-				ret.(i).(j) <- rgbToGreyScale (rgbint_to_color image.matrix.(i).(j))
+				ret.(i).(j) <- rgbToGreyScale image.matrix.(i).(j)
 			done;
 		done;
 		({height = h; width = w; matrix = ret} : bw_image);;
